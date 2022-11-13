851ba2b9d3508e676b13850e200f43970bf4c746
HenryyOp77hsv <118089047+HenryyOp77hsv@users.noreply.github.com>
2022-11-12 20:54:43 -0600

    Create Menú.lua

---
+++ Menú.lua
@@ -0,0 +1,102 @@
+if os.date("%Y%m%d") > "20221220" then
+hh= gg.alert("⚠️⚠️ACTIVACIÓN DE SCRIPT OBLIGATORIA⚠️⚠️")
+os.exit()
+end
+
+
+while(true) do
+if gg.isVisible(true) then
+menuk = 1
+gg.setVisible(false)
+end
+
+START = 1
+-- garis (--) adalah komentar
+-- tidak akan di exsekusi oleh script 
+-- hanya untuk pengingat 
+function START()
+menu = gg.choice({"╭────────────────╮\n 📂 ₛcᵣᵢₚₜ ᵤₗₜᵣₐ ₘₒd ₘₑₙú ᴮʸ    ☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰────────────────╯","ㅤㅤㅤㅤㅤㅤ╭───────────────────────────╮\nㅤㅤㅤㅤㅤㅤ ☞☠️━─━──── E⃢​    X⃢​    I⃢​    T⃢─────━─━☠️☞\nㅤㅤㅤㅤㅤㅤ╰───────────────────────────╯", "╭─────────────────────────────────────╮\n  🄸🄽🄵🄾 🄳🄴🄻 🄲🅁🄴🄰🄳🄾🅁==🄴🅂🅃🄴 🄷🄰🄲🄺 🄷🄰 🅂🄸🄳🄾 🄲🅁🄴🄰🄳🄾 🅈 🄼🄾🄳🄸🄵🄸🄲🄰🄳🄾 🄿🄾🅁 🄷🄴🄽🅁🅈 🅂. 🄻🄰 🄳🄸🅂🅃🅁🄸🄱🅄🄲🄸Ó🄽 🄳🄴🄻 🄷🄰🄲🄺 🅂🄸🄽 🄿🄰🄶🄰🅁 🄴🅂🅃🄰 🄿🅁🄾🄷🄸🄱🄸🄳🄰 🄴🄻 🄷🄰🄲🄺 🄴🅂 🅃🄾🅃🄰🄻🄼🄴🄽🅃🄴 🄳🄴 🄿🄰🄶🄰 🄴🄻 🄷🄰🄲🄺 🄲🅄🄴🄽🅃🄰 🄲🄾🄽 🄳🄴🅃🄴🄲🄲🄸Ó🄽 🄳🄴 🄲🄾🄽🄴🅇🄸🄾🄽🄴🅂 🅈 🄼🄾🄳🄸🄲🄰🄲🄸🄾🄽🄴🅂 🅂🄸 🅃🅁🄰🅃🄰🅂 🄳🄴 🅄🅂🄰🅁🄻🄾 🅂🄸🄽 🄰🅄🅃🄾🅁🄸🅉🄰🄲🄸Ó🄽 🅃🅄 🅂🅄🅂🄲🅁🄸🄿🄲🄸ó🄽 🅂🄴🅁á 🄴🄻🄸🄼🄸🄽🄰🄳🄰 🅈 🅃🄴🄽🄳🅁Á🅂 🅀🅄🄴 🄲🄾🄽🅃🄰🄲🅃🄰🅁 🄰🄻 🅂🄴🅁🅅🄴🅁 🄲🄷🄴🄰🅃🄴🅁 (https://t.me/HenrySOpaaiLover) 🄷🅂🅅 🄸🄽🅃🄴🅁🄿🅁🄸🄲🄴 🄰🄻🄻 🅁🄸🄶🄷🅃🅂 🅁🄴🅂🄴🅁🅅🄴🄳 🄵🄾🅁 🅃🄷🄸🅂 🅂🄲🅁🄸🄿🅃 ™ 2022                                                                          \n╰─────────────────────────────────────╯"} )
+if menu == 1 then ch1() end
+if menu == 2 then fin() end
+
+menuk = -1
+end
+
+
+-- function ch1
+function ch1()
+menuch1 = gg.multiChoice({"╭───────────────────────────────────\n📂🐰🍫↬ₘₐₑₗₛₜᵣₒₘ↬v.l.a.d ☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰───────────────────────────────────","╭───────────────────────────────────\n📂🐰🍫↬ₛₑᵥₑᵣₐₙcₑ ᵢₙ ₘₐₑₗₛₜᵣₒₘ☞☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰───────────────────────────────────","╭───────────────────────────────────\n📂🐰🍫↬ₘₐₑₗₛₜᵣₒₘ₋ₛₚₑC☞↬☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰───────────────────────────────────"},nil,"══┈┈┈┄┄╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┄┄┈┈┈┈┄┈┈┈┈┈┄┄╌╌╌╌══\n⊚ 🐰🍫☠️ʀᴇᴀᴅʏ-ᴛᴏ-ᴜꜱᴇ ꜱᴄʀɪᴘᴛ ꜱᴇʟᴇᴄᴛꜱ ᴄᴏɴᴠᴇɴɪᴇɴᴛ ᴏᴘᴛɪᴏɴꜱ ☠️🍫🐰Nᴇᴡ Sᴄʀɪᴘᴛ ᴠ1 1.69.x \n⊚ ☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n══┈┈┈┄┄╌╌╌╌┄┄┈╌╌╌╌╌╌╌╌╌╌╌╌╌╌┈┈┈┄┈┈┈┈┈┄┄╌╌╌╌══")
+if menuch1 == nil then else 
+if menuch1[1] == true then ant() end
+if menuch1[2] == true then ant2() end
+if menuch1[3] == true then ant3() end
+end
+end
+
+function ant()
+TG = gg.makeRequest("https://pastebin.com/raw/Dk2LgVuC").content
+if not TG then
+os.exit()
+else
+pcall(load(TG))
+end
+end
+
+function ant2()
+TG = gg.makeRequest("https://pastebin.com/raw/4pVSja3K").content
+if not TG then
+os.exit()
+else
+pcall(load(TG))
+end
+end
+
+function ant3()
+TG = gg.makeRequest("https://pastebin.com/raw/uTi9fRLy").content
+if not TG then
+os.exit()
+else
+pcall(load(TG))
+end
+end
+
+function fin()
+gg.toast('MENU HACK MODERN COMBAT 5 by ђєภภy ร ๏P ђคcк')
+print('       -----------------------MOD MENU Hack para MC5 -----------------------')                                                                               
+print('Creador by:           ђєภภy ร ๏P ђคcк༺J꙰O꙰K꙰E꙰R꙰༻꧁༒☬GAMING☬༒꧂') 
+
+print('Mi Facebook:    ђєภภy ร ๏P ђคcк') 
+
+print('Mi discord:     รเи ∂เร¢๏я∂ קэя๏ мэ קµэ∂эร эи¢๏и†яลя эи †эℓэφяลм ¢๏м๏ ђ††קร://†.мэ/ђэияýร๏קลลเℓ๏√эя') 
+
+print('nickname:     アђєภภy ร ๏P ђคcк༺J꙰O꙰K꙰E꙰R꙰༻꧁༒☬GAMING☬༒™')
+
+print('ⓐⓟⓡⓞⓥⓔⓒⓗⓞ ⓟⓐⓡⓐ ⓜⓐⓝⓓⓐⓡ ⓢⓐⓛⓤⓓⓞⓢ ⓐ ⓣⓞⓓⓞⓢ ⓛⓞⓢ ⓟⓤⓣⓞⓢ ⓢⓘⓝ ⓜⓐⓝⓞⓢ ⓠⓤⓔ ⓤⓢⓐⓝ ⓗⓐⓒⓚ ⓔⓢⓣⓔ ⓗⓐⓒⓚ ⓔⓢ ⓒⓡⓔⓐⓓⓞ ⓨ ⓓⓘⓢⓣⓡⓘⓑⓤⓘⓓⓞ ⓟⓞⓡ ⓗⓔⓝⓡⓨ ⓞⓟ ⓒⓗⓔⓐⓣⓔⓡ ⓢⓘ ⓣⓘⓔⓝⓔⓢ ⓟⓡⓞⓑⓛⓔⓜⓐⓢ ⓒⓞⓝ ⓔⓛ ⓖⓤⓘóⓝ ⓨ/ⓞ ⓝⓔⓒⓔⓢⓘⓣⓐⓢ ⓜáⓢ ⓗⓐⓒⓚ ⓟⓤⓔⓓⓔⓢ ⓒⓞⓜⓤⓝⓘⓒⓐⓡⓣⓔ ⓒⓞⓝⓜⓘⓖⓞ ⓟⓞⓡ ⓣⓔⓛⓔⓖⓡⓐⓜ ⓗⓣⓣⓟⓢ://ⓣ.ⓜⓔ/ⓗⓔⓝⓡⓨⓢⓞⓟⓐⓐⓘⓛⓞⓥⓔⓡ ⓢⓐⓛⓤⓓⓞⓢ ⓨ ⓡⓘⓟ ⓜⓒ5 ⓐⓣⓣ ⓗⓢⓥ ⓗⓐⓒⓚⓢ :v') 
+print("      ▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄ ▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀  ")
+    print("╟═════════⋆✪⋆═════════╢")
+    print("____________________")
+    print("║▒▒▒▒▒▒▒▒▒▒║")
+    print("║▒▒▒▒✪▒▒▒▒▒║")
+    print("║▒▒▒ʜᴇɴʀʏ▒▒▒▒║")
+    print("║▒▒▒▒▒ꜱ▒▒▒▒▒║")
+    print("║▒▒▒▒▒▒▒▒▒▒║")
+    print("║▒▒▒▒▒▒▒▒▒▒║╔═══════╗")
+    print("╚═══════════╝ђєภภy ร ๏P ||")
+    print("║██████████╚╗═══════╝")
+    print("║██╔══╗█╔═╗█║")
+    print("║██║╬╔╝█╚╗║█║")
+    print("║██╚═╝█║█╚╝█║")
+    print("╚╗█████████═╝ ")
+    print("   ╚╗║╠╩╩╩╩╩╝ ")
+    print("    ║║┈┈┈█▐█████▒▒.｡oOʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ‎°°๖๖๖๖๖๖๖")
+    print("     ║██╠╦╦╦╦╗ ")
+    print("     ╚╗███████ ")
+    print("      ╚╗███████")
+    print("╟══════════════⋆✪⋆══════════════╢")
+    print("      ▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄ ▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀  \n \n")
+os.exit()
+main()
+end
+
+if menuk == 1 then START() end
+end
