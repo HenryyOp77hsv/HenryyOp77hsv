088bc6f8baa3f84c395b34f4d17e4112f090c6a9
MChAck Game <118089047+HenryyOp77hsv@users.noreply.github.com>
2022-12-11 10:33:58 -0600

    Update Menú.lua

--- Menú.lua
+++ Menú.lua
@@ -120,7 +120,7 @@ end
 
 
 function Henry9() 
-TG = gg.makeRequest("https://raw.githubusercontent.com/HenryyOp77hsv/HenryyOp77hsv/main/IMPS.lua").content
+TG = gg.makeRequest("https://raw.githubusercontent.com/HenryyOp77hsv/HenryyOp77hsv/main/New%20zombie.lua").content
 if not TG then
 os.exit()
 else
