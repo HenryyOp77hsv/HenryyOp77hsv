e502152bf15354508a8d3ac37319534cc4c203c0
MChAck Game <118089047+HenryyOp77hsv@users.noreply.github.com>
2022-12-28 12:12:54 -0600

    Create No weapon.lua

---
+++ No weapon.lua
@@ -0,0 +1,295 @@
+
+
+
+gg.alert("🆂🅲🆁🅸🅿🆃 🅱🆈 🅷🅴🅽🆁🆈 🆂 🐰🍫☠️ₜₑₛₜ ᵥᵢₚ ₚₑₐCₕY ₑᵥₑₙₜ ☠️🍫🐰ㅤㅤㅤㅤㅤ ㅤ🇼​🇪​🇱​🇨​🇴​🇲​🇪​ 🇹​🇴​ 🇭​🇪​🇳​🇷​🇾​ 🇸​ 🇲​🇴​🇩​ 🇸​🇨​🇷​🇮​🇵​🇹​ 🇭​🇦​🇨​🇰​ ══┈┈┈┄┄╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┄┄┈┈┈┈┄┈┈┈┈┈┄┄╌╌╌╌══\n⊚ 🐰🍫☠️ʀᴇᴀᴅʏ-ᴛᴏ-ᴜꜱᴇ ꜱᴄʀɪᴘᴛ ꜱᴇʟᴇᴄᴛꜱ ᴄᴏɴᴠᴇɴɪᴇɴᴛ ᴏᴘᴛɪᴏɴꜱ ☠️🍫🐰Nᴇᴡ Sᴄʀɪᴘᴛ ᴠ1 1.69.x \n⊚ ☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n══┈┈┈┄┄╌╌╌╌┄┄┈╌╌╌╌╌╌╌╌╌╌╌╌╌╌┈┈┈┄┈┈┈┈┈┄┄╌╌╌╌══")
+gg.toast("ꈜꏳꌅ꒐ꉣꋖ ꏳꌅꁲꑒꐔ ꋖꌈꃔ꒐ꈜ꒐ꁲꃔꁲ ꅐꑀ꒒ꏳꊿꂵ ꋖꊿ ꃳꑀꐔ ꂵꐔ ꁝꁲꏳꈵ ꃳꐔ ꌅꑀꏳꊿꃔ")
+gg.toast ("WAITE TO CONECTED ᔕHenry S Op,")
+---LOADING STEP-[2] (💥Henry S Pro op hack💥)---
+
+while(true) do
+if gg.isVisible(true) then
+menuk = 1
+gg.setVisible(false)
+end
+
+START = 1
+-- garis (--) adalah komentar
+-- tidak akan di exsekusi oleh script 
+-- hanya untuk pengingat 
+function START()
+menu = gg.choice({"╭────────────────╮\n📂ₛcᵣᵢₚₜ ᵤₗₜᵣₐ ₘₒd ₘₑₙú ᴮʸ    ☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰────────────────╯","╭───────────────────────────╮\n ☞☠️━─━──── E⃢​    X⃢​    I⃢​    T⃢─────━─━☠️☞\n╰───────────────────────────╯","╭─────────────────────────────────────╮\n       🄸🄽🄵🄾 🄳🄴🄻 🄲🅁🄴🄰🄳🄾🅁==🄴🅂🅃🄴 🄷🄰🄲🄺 🄷🄰 🅂🄸🄳🄾 🄲🅁🄴🄰🄳🄾 🅈 🄼🄾🄳🄸🄵🄸🄲🄰🄳🄾 🄿🄾🅁 🄷🄴🄽🅁🅈 🅂. 🄻🄰 🄳🄸🅂🅃🅁🄸🄱🅄🄲🄸Ó🄽 🄳🄴🄻 🄷🄰🄲🄺 🅂🄸🄽 🄿🄰🄶🄰🅁 🄴🅂🅃🄰 🄿🅁🄾🄷🄸🄱🄸🄳🄰 🄴🄻 🄷🄰🄲🄺 🄴🅂 🅃🄾🅃🄰🄻🄼🄴🄽🅃🄴 🄳🄴 🄿🄰🄶🄰 🄴🄻 🄷🄰🄲🄺 🄲🅄🄴🄽🅃🄰 🄲🄾🄽 🄳🄴🅃🄴🄲🄲🄸Ó🄽 🄳🄴 🄲🄾🄽🄴🅇🄸🄾🄽🄴🅂 🅈 🄼🄾🄳🄸🄲🄰🄲🄸🄾🄽🄴🅂 🅂🄸 🅃🅁🄰🅃🄰🅂 🄳🄴 🅄🅂🄰🅁🄻🄾 🅂🄸🄽 🄰🅄🅃🄾🅁🄸🅉🄰🄲🄸Ó🄽 🅃🅄 🅂🅄🅂🄲🅁🄸🄿🄲🄸ó🄽 🅂🄴🅁á 🄴🄻🄸🄼🄸🄽🄰🄳🄰 🅈 🅃🄴🄽🄳🅁Á🅂 🅀🅄🄴 🄲🄾🄽🅃🄰🄲🅃🄰🅁 🄰🄻 🅂🄴🅁🅅🄴🅁 🄲🄷🄴🄰🅃🄴🅁 (🄷🅃🅃🄿🅂://🅃.🄼🄴/🄷🄴🄽🅁🅈🅂🄾🄿🄰🄰🄸🄻🄾🅅🄴🅁) 🄷🅂🅅 🄸🄽🅃🄴🅁🄿🅁🄸🄲🄴 🄰🄻🄻 🅁🄸🄶🄷🅃🅂 🅁🄴🅂🄴🅁🅅🄴🄳 🄵🄾🅁 🅃🄷🄸🅂 🅂🄲🅁🄸🄿🅃 ™ 2022                                                                          \n╰─────────────────────────────────────╯"} )
+if menu == 1 then ch1() end
+if menu == 2 then fin() end
+
+menuk = -1
+end
+
+
+-- function ch1
+function ch1()
+menuch1 = gg.multiChoice({"╭────────────────╮\n📂FᵤₙCₜᵢₒₙ FᵣₑₑZₑₚᵢₛₜₒₗ(ᴸᵒᵇᵇʸ)☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰────────────────╯","╭────────────────╮\n📂ₙₒWₑₐₚₒₙ ᵢₙ ₘₐₑₗₛₜᵣₒₘ(ᴵⁿ ᵍᵃᵐᵉ) ☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰────────────────╯","╭────────────────╮\n📂ₘₐₑₗₛₜᵣₒₘ ₙₒWₑₐₚₒₙ (ₗₒBBY).  ☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰────────────────╯","╭────────────────╮\n📂ₘₛg₃₃↬FᵣₑₑZₑₚᵢₛₜₒₗ(ᴵⁿᵍᵃᵐᵉ)☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰────────────────╯","╭────────────────╮\n📂🅰🅸🅼🅱🅾🆃 ☞ (ᴵⁿ ᵍᵃᵐᵉ)☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰────────────────╯","╭────────────────╮\n📂ˢᴵˣᵀᴴ ˢᴱᴺˢᴱ ᶜᴼᴿᴱ ☞(ₗₒBBY)☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n╰────────────────╯"},nil,"🇭​🇪​🇳​🇷​🇾​ 🇸​ 🇲​🇴​🇩​ 🇸​🇨​🇷​🇮​🇵​🇹​ 🇭​🇦​🇨​🇰​ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🐰🍫☠️ʀᴇᴀᴅʏ-ᴛᴏ-ᴜꜱᴇ ꜱᴄʀɪᴘᴛ ꜱᴇʟᴇᴄᴛꜱ ᴄᴏɴᴠᴇɴɪᴇɴᴛ ᴏᴘᴛɪᴏɴꜱ ultra millonary bug for peachy event☠️🍫🐰.")
+if menuch1 == nil then else 
+if menuch1[1] == true then ant() end
+if menuch1[2] == true then ant2() end
+if menuch1[3] == true then ant3() end
+if menuch1[4] == true then ant4() end
+if menuch1[5] == true then ant5() end
+if menuch1[6] == true then ant6() end
+
+end
+end
+
+function ant()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("59.0;134.0;1033.0:73", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("59", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+if v.flags == gg.TYPE_FLOAT then
+v.value = "-172"
+v.freeze = true
+end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("34.34999847412F;1033.0F;4130.0F:221", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("34.34999847412", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "35.7"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults() 
+gg.toast('🥶FREEZER = ON🥶')
+end
+
+
+
+function ant2()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("260845;66;-1596977136:9", gg.TYPE_DWORD)
+gg.refineNumber("-1596977136", gg.TYPE_DWORD)
+revert = gg.getResults(1)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "-1993342976"
+v.freeze = true
+end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast("ₙₒWₑₐₚₒₙ CODE FREEZE 🥶")
+end
+
+
+
+function ant3() 
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('"51.0F;233377.0F;233568.0F:41"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber('"51"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll('"1.0e10"', gg.TYPE_FLOAT)
+
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = '"1.0e10"'
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('"84.0;134.0;54.0;159.0;114.0;40.0;36.0:25"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.processResume()
+gg.refineNumber('"54"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.processResume()
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll('"-171"', gg.TYPE_FLOAT)
+
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = '"-179"'
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('"35.0;69.0;4130.0;1029.0:37"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber('"69"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll('"99999999999999999"', gg.TYPE_FLOAT)
+
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = '"9.9999998e16"'
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('"20034.0;35.0;36.5:13"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.processResume()
+gg.refineNumber('"35.0"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll('"999999999999999"', gg.TYPE_FLOAT)
+
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = '"9.9999999e14"'
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.toast('Hack ₙₒWₑₐₚₒₙ=ON')
+end
+
+function ant4() 
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("-2.64835626e-23;-1.0;40.0:65", gg.TYPE_FLOAT)
+gg.refineNumber("-2.64835626e-23", gg.TYPE_FLOAT)
+revert = gg.getResults(1)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+if v.flags == gg.TYPE_FLOAT then
+v.value = "-5.39260972e-33"
+v.freeze = true
+end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast('🥶FREEZER = ON🥶 OPTION ACTIVATION IN GAME')
+end
+
+
+function ant5() 
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0.005;0.180", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("9999.8", gg.TYPE_FLOAT)
+if revert ~= nil then gg.setValues(revert) end
+revert = gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("9999.8", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Tiro na cabeça")
+
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("-0.0001", gg.TYPE_FLOAT)
+gg.getResults(10)
+gg.editAll("9999.8", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Tiro através da parede")
+end
+
+
+function ant6() 
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("1.20000004768F;1000.0F;1000.0F:73", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1.20000004768", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(6, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("-5", gg.TYPE_FLOAT)
+
+revert = gg.getResults(6, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(6, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "-5"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults() 
+
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("51295.4375F;40.0F;51295.4375F:769", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("40", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+revert = gg.getResults(17, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(17, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "999"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults() 
+gg.toast('𝐔𝐋𝐓𝐑𝐀 𝐒𝐈𝐗𝐓𝐇 𝐒𝐄𝐍𝐒𝐄 𝐎𝐍')
+end
+
+function fin()
+gg.toast('MENU HACK MODERN COMBAT 5 by ђєภภy ร ๏P ђคcк')
+print('       -----------------------MOD MENU Hack para MC5 -----------------------')                                                                               
+print('Creador by:           ђєภภy ร ๏P ђคcк༺J꙰O꙰K꙰E꙰R꙰༻꧁༒☬GAMING☬༒꧂') 
+
+print('Mi Facebook:    ђєภภy ร ๏P ђคcк') 
+
+print('Mi discord:     รเи ∂เร¢๏я∂ קэя๏ мэ קµэ∂эร эи¢๏и†яลя эи †эℓэφяลм ¢๏м๏ ђ††קร://†.мэ/ђэияýร๏קลลเℓ๏√эя') 
+
+print('nickname:     アђєภภy ร ๏P ђคcк༺J꙰O꙰K꙰E꙰R꙰༻꧁༒☬GAMING☬༒™')
+
+print('ⓐⓟⓡⓞⓥⓔⓒⓗⓞ ⓟⓐⓡⓐ ⓜⓐⓝⓓⓐⓡ ⓢⓐⓛⓤⓓⓞⓢ ⓐ ⓣⓞⓓⓞⓢ ⓛⓞⓢ ⓟⓤⓣⓞⓢ ⓢⓘⓝ ⓜⓐⓝⓞⓢ ⓠⓤⓔ ⓤⓢⓐⓝ ⓗⓐⓒⓚ ⓔⓢⓣⓔ ⓗⓐⓒⓚ ⓔⓢ ⓒⓡⓔⓐⓓⓞ ⓨ ⓓⓘⓢⓣⓡⓘⓑⓤⓘⓓⓞ ⓟⓞⓡ ⓗⓔⓝⓡⓨ ⓞⓟ ⓒⓗⓔⓐⓣⓔⓡ ⓢⓘ ⓣⓘⓔⓝⓔⓢ ⓟⓡⓞⓑⓛⓔⓜⓐⓢ ⓒⓞⓝ ⓔⓛ ⓖⓤⓘóⓝ ⓨ/ⓞ ⓝⓔⓒⓔⓢⓘⓣⓐⓢ ⓜáⓢ ⓗⓐⓒⓚ ⓟⓤⓔⓓⓔⓢ ⓒⓞⓜⓤⓝⓘⓒⓐⓡⓣⓔ ⓒⓞⓝⓜⓘⓖⓞ ⓟⓞⓡ ⓣⓔⓛⓔⓖⓡⓐⓜ ⓗⓣⓣⓟⓢ://ⓣ.ⓜⓔ/ⓗⓔⓝⓡⓨⓢⓞⓟⓐⓐⓘⓛⓞⓥⓔⓡ ⓢⓐⓛⓤⓓⓞⓢ ⓨ ⓡⓘⓟ ⓜⓒ5 ⓐⓣⓣ ⓗⓢⓥ ⓗⓐⓒⓚⓢ :v') 
+print("      ▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄ ▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀  ")
+    print("╟═════════⋆✪⋆═════════╢")
+    print("____________________")
+    print("║▒▒▒▒▒▒▒▒▒▒║")
+    print("║▒▒▒▒✪▒▒▒▒▒║")
+    print("║▒▒▒ʜᴇɴʀʏ▒▒▒▒║")
+    print("║▒▒▒▒▒ꜱ▒▒▒▒▒║")
+    print("║▒▒▒▒▒▒▒▒▒▒║")
+    print("║▒▒▒▒▒▒▒▒▒▒║╔═══════╗")
+    print("╚═══════════╝ђєภภy ร ๏P ||")
+    print("║██████████╚╗═══════╝")
+    print("║██╔══╗█╔═╗█║")
+    print("║██║╬╔╝█╚╗║█║")
+    print("║██╚═╝█║█╚╝█║")
+    print("╚╗█████████═╝ ")
+    print("   ╚╗║╠╩╩╩╩╩╝ ")
+    print("    ║║┈┈┈█▐█████▒▒.｡oOʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ‎°°๖๖๖๖๖๖๖")
+    print("     ║██╠╦╦╦╦╗ ")
+    print("     ╚╗███████ ")
+    print("      ╚╗███████")
+    print("╟══════════════⋆✪⋆══════════════╢")
+    print("      ▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄ ▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀  \n \n")
+os.exit()
+main()
+end
+
+if menuk == 1 then START() end
+end
